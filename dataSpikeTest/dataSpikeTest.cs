869d895e4bc04fc539323376b6b4d05cfb8c6f94
João Victor de Souza <j.victor@sinova.com.br>
2022-06-28 22:27:19 -0300

    [Add]: project 'dataSpikeTest'

--- dataSpikeTest/dataSpikeTest.cs
+++ dataSpikeTest/dataSpikeTest.cs
@@ -1,1 +1,15 @@
-﻿
+﻿namespace dataSpike
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("https://api.henrikdev.xyz");
+
+            var response = await client.GetAsync("valorant/v1/account/fuba/joao");
+            var content = await response.Content.ReadAsStringAsync();
+            Console.WriteLine(content);
+        }
+    }
+}
