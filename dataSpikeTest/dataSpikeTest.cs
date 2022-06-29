7c8a11f02842c04297766c31dabfbbea3cf8d44b
João Victor de Souza <j.victor@sinova.com.br>
2022-06-28 23:11:05 -0300

    [Test]: 'Account' class

--- dataSpikeTest/dataSpikeTest.cs
+++ dataSpikeTest/dataSpikeTest.cs
@@ -1,15 +1,35 @@
-﻿namespace dataSpike
+﻿using Entities;
+using Newtonsoft.Json;
+
+namespace dataSpike
 {
     class Program
     {
+        private static HttpClient client = new HttpClient();
+
+        private static readonly string baseUri = "https://api.henrikdev.xyz";
+        private static readonly string route = "valorant";
+
+        private static readonly string name = "fuba";
+        private static readonly string gameTag = "joao";
+        private static readonly string region = "na";
+
+        private static readonly string v1 = "v1";
+        private static readonly string v2 = "v2";
+        private static readonly string v3 = "v3";
+
         static async Task Main(string[] args)
         {
-            HttpClient client = new HttpClient();
-            client.BaseAddress = new Uri("https://api.henrikdev.xyz");
+            Console.WriteLine(await GetAccountAsync());
+        }
 
-            var response = await client.GetAsync("valorant/v1/account/fuba/joao");
+        public async static Task<string> GetAccountAsync()
+        {       
+            client.BaseAddress = new Uri(baseUri);
+            var response = await client.GetAsync($"{route}/{v1}/account/{name}/{gameTag}");
             var content = await response.Content.ReadAsStringAsync();
-            Console.WriteLine(content);
+            var account = JsonConvert.DeserializeObject<Account>(content);
+            return account != null? account.ToString() : "Account object returned null.";
         }
     }
 }
