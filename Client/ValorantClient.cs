dff66bf38157d496eb70c5115cf4c61199b92a7d
João Victor de Souza <j.victor@sinova.com.br>
2022-06-29 23:41:25 -0300

    [Update]: added method 'GetAccountAsync()' in 'ValorantClient' class

--- Client/ValorantClient.cs
+++ Client/ValorantClient.cs
@@ -1,7 +1,33 @@
-﻿namespace Client
+﻿using Entities;
+using Newtonsoft.Json;
+
+namespace Client
 {
     public class ValorantClient
     {
+        private static HttpClient client = new();
+        private const string baseUri = "https://api.henrikdev.xyz";
+        private const string route = "valorant";
+        private const string version1 = "v1";
+        private const string version2 = "v2";
+        private const string version3 = "v3";
 
+        public static string Name { get; private set; }
+        public static string Tag { get; private set; }
+
+        public ValorantClient(string name, string tag)
+        {
+            Name = name;
+            Tag = tag;
+        }
+
+        public async Task<Account> GetAccountAsync()
+        {
+            client.BaseAddress = new Uri(baseUri);
+            var response = await client.GetAsync($"{route}/{version1}/account/{Name}/{Tag}");
+            var content = await response.Content.ReadAsStringAsync();
+            var account = JsonConvert.DeserializeObject<Account>(content);
+            return account;
+        }
     }
 }
--- dataSpikeTest/dataSpikeTest.cs
+++ dataSpikeTest/dataSpikeTest.cs
@@ -1,35 +1,17 @@
-﻿using Entities;
+﻿using Client;
+using Entities;
 using Newtonsoft.Json;
 
 namespace dataSpike
 {
     class Program
     {
-        private static HttpClient client = new();
+        private static ValorantClient valorantClient = new("fuba", "joao");
 
-        private static readonly string baseUri = "https://api.henrikdev.xyz";
-        private static readonly string route = "valorant";
-
-        private static readonly string name = "fuba";
-        private static readonly string gameTag = "joao";
-        private static readonly string region = "na";
-
-        private static readonly string v1 = "v1";
-        private static readonly string v2 = "v2";
-        private static readonly string v3 = "v3";
-
-        static async Task Main(string[] args)
+        async static Task Main(string[] args)
         {
-            Console.WriteLine(await GetAccountAsync());
-        }
-
-        public async static Task<string> GetAccountAsync()
-        {       
-            client.BaseAddress = new Uri(baseUri);
-            var response = await client.GetAsync($"{route}/{v1}/account/{name}/{gameTag}");
-            var content = await response.Content.ReadAsStringAsync();
-            var account = JsonConvert.DeserializeObject<Account>(content);
-            return account != null? account.ToString() : "Account object returned null.";
+            var account = await valorantClient.GetAccountAsync();
+            Console.WriteLine(account.ToString());
         }
     }
 }
