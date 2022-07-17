c9a5a94df7d0047f7e39d8e168a79932ec7732ed
João Victor de Souza <j.victor@sinova.com.br>
2022-07-17 00:41:05 -0300

    [Add]: 'Matches' class

---
+++ Entities/Matches.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+
+namespace Entities
+{
+    public partial class Matches
+    {
+        [JsonProperty("status")]
+        private long Status { get; set; }
+
+        [JsonProperty("data")]
+        private List<Match>? Data { get; set; }
+
+        public long GetStatus() => Status;
+        public List<Match> GetMatches() => Data;
+
+        public override string ToString()
+        {
+            string toString = $"Status: {GetStatus()}\n";         
+            if (Data != null) foreach (Match match in Data) toString += match.ToString();
+
+            return toString;
+        }
+    }
+}
+
